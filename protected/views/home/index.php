c0d914481b1c55cc25c0ab43119da95c9b3849ce
Pandu Bagoes Pradana <pandubaguspr@gmail.com>
2019-08-22 16:36:08 +0700

    krg footer, slider

--- protected/views/home/index.php
+++ protected/views/home/index.php
@@ -1,228 +1,134 @@
-<?php
-$mod_kategori = [
-    1 => [
-        'gambar' => 'furniture.jpg',
-        'judul' => 'Furniture',
-    ],
-    [
-        'gambar' => 'houseware.jpg',
-        'judul' => 'Houseware',
-    ],
-    [
-        'gambar' => 'tableware.jpg',
-        'judul' => 'Tableware',
-    ],
-    [
-        'gambar' => 'container.jpg',
-        'judul' => 'Container',
-    ]
-];
-?>
-
-
-
 <section class="home-sec-1">
-	<div class="prelative container">
-		<div class="row">
-			<div class="title-top">
-				<p>Kategori Favorit Produk Plastik Lucky Star</p>
-			</div>
-		</div>
-		<div class="row">
-			<?php foreach($mod_kategori as $key => $value): ?>
-				<div class="col-md-15">
-					<div class="image"><img class="w-100" src="<?php echo $this->assetBaseurl; ?><?php echo $value['gambar'] ?>" alt=""></div>
-					<div class="title">
-						<p><?php echo $value['judul'] ?></p>
-					</div>
-					<div class="lihat_kat">
-						<a href="#">lihat kategori</a>
-					</div>
-				</div>
-			<?php endforeach ?>
-		</div>
-	</div>
+    <div class="prelative container">
+        <div class="row">
+            <div class="col-md-60">
+                <div class="box-content top">
+                    <div class="title">
+                        <h4>Reich Architecture & Hardware Accessories Category</h4>
+                    </div>
+                    <div class="subtitle">
+                        <p>Click here to view all products.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-30">
+                <div class="box-content">
+                    <div class="image">
+                        <img src="<?php echo $this->assetBaseurl; ?>home1.jpg" alt="">
+                        <p>Aluminium related solution</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-30">
+                <div class="box-content">
+                    <div class="image">
+                        <img src="<?php echo $this->assetBaseurl; ?>home2.jpg" alt="">
+                        <p>Digital door locks</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-20">
+                <div class="box-content">
+                    <div class="image bottom">
+                        <img src="<?php echo $this->assetBaseurl; ?>home3.jpg" alt="">
+                        <p>Slim drawer solution</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-20">
+                <div class="box-content">
+                    <div class="image bottom">
+                        <img src="<?php echo $this->assetBaseurl; ?>home4.jpg" alt="">
+                        <p>Glass related solution</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-20">
+                <div class="box-content">
+                    <div class="image bottom">
+                        <img src="<?php echo $this->assetBaseurl; ?>home5.jpg" alt="">
+                        <p>Wood related solution</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </section>
 
-<?php
-$mod_fav = [
-    1 => [
-        'gambar' => 'favorite1.jpg',
-        'judul' => 'Baki Anyam Besar',
-    ],
-    [
-        'gambar' => 'favorite2.jpg',
-        'judul' => 'Baki Anyam Kecil',
-    ],
-    [
-        'gambar' => 'favorite3.jpg',
-        'judul' => 'Keranjang Anyam Bundar',
-    ],
-    [
-        'gambar' => 'favorite4.jpg',
-        'judul' => 'Keranjang Anyam Besar',
-    ],
-    [
-        'gambar' => 'favorite5.jpg',
-        'judul' => 'Pel pelan plastik',
-    ],
-    [
-        'gambar' => 'favorite6.jpg',
-        'judul' => 'Pemeras Jeruk',
-    ],
-    [
-        'gambar' => 'favorite7.jpg',
-        'judul' => 'Baki Anyam Besar',
-    ],
-    [
-        'gambar' => 'favorite8.jpg',
-        'judul' => 'Baki Anyam Kecil',
-    ],
-    [
-        'gambar' => 'favorite9.jpg',
-        'judul' => 'Keranjang Anyam Bundar',
-    ],
-    [
-        'gambar' => 'favorite10.jpg',
-        'judul' => 'Keranjang Anyam Besar',
-    ],
-    [
-        'gambar' => 'favorite11.jpg',
-        'judul' => 'Pel pelan plastik',
-    ],
-    [
-        'gambar' => 'favorite12.jpg',
-        'judul' => 'Pemeras Jeruk',
-    ],
-    [
-        'gambar' => 'favorite13.jpg',
-        'judul' => 'Baki Anyam Besar',
-    ],
-    [
-        'gambar' => 'favorite14.jpg',
-        'judul' => 'Baki Anyam Kecil',
-    ],
-    [
-        'gambar' => 'favorite15.jpg',
-        'judul' => 'Keranjang Anyam Bundar',
-    ],
-    [
-        'gambar' => 'favorite16.jpg',
-        'judul' => 'Keranjang Anyam Besar',
-    ],
-    [
-        'gambar' => 'favorite17.jpg',
-        'judul' => 'Pel pelan plastik',
-    ],
-    [
-        'gambar' => 'favorite18.jpg',
-        'judul' => 'Pemeras Jeruk',
-    ]
-];
-?>
-
 <section class="home-sec-2">
-	<div class="prelative container">
-		<div class="row">
-			<div class="col-md-60">
-				<div class="title">
-					<p>Produk Plastik Favorit Lucky Star</p>
-				</div>
-			</div>
-		</div>
-		<div class="row">
-			<?php foreach($mod_fav as $key => $value): ?>
-				<div class="col-md-10">
-					<div class="box-content">
-						<div class="image"><img class="w-100" src="<?php echo $this->assetBaseurl; ?><?php echo $value['gambar'] ?>" alt=""></div>
-						<div class="judul">
-							<p><?php echo $value['judul'] ?></p>
-						</div>
-						<div class="lihat"><a href="#">lihat produk</a></div>
-					</div>
-				</div>
-			<?php endforeach ?>
-		</div>
-		<div class="lihat-semua">
-			<a href="#">lihat semua produk</a>
-		</div>
-	</div>
+    <div class="prelative container2">
+        <div class="row">
+            <div class="col-md-30"></div>
+            <div class="col-md-30">
+                <div class="box-content">
+                    <div class="caption">
+                        <p>Want to become our partner in distributing<br><b>Reich hardware & accessories products?</b></p>
+                    </div>
+                    <div class="subtitle">
+                        <p>Reich Architectural & Interior accessories will bring you an excellent plan to market and distribute smart and technological solution products. Expect a profitable business plan that can help construct and build your relationship with modern customers and furniture makers. Come and join us, our representative will be ready to answer your inquiries.</p>
+                    </div>
+                    <div class="click">
+                        <p><a href="#">CLICK HERE</a></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </section>
 
 <section class="home-sec-3">
-	<div class="prelative container">
-		<div class="row">
-			<div class="col-md-60">
-				<div class="title">
-					<p>Dapatkan Koleksi Lengkap Produk Plastik Lucky Star</p>
-				</div>
-				<div class="download">
-					<a href="#"><i></i>Download Katalog</a>
-				</div>
-			</div>
-		</div>
-	</div>
-</section>
-
-<?php
-$mod_blog = [
-    1 => [
-        'gambar' => 'furniture.jpg',
-        'judul' => 'Cara mengurangi limbah plastik adalah dengan tidak membuangnya',
-        'kat' => 'artikel'
-    ],
-    [
-        'gambar' => 'houseware.jpg',
-        'judul' => 'Lucky Star berpartisi meramaikan Java Big Bang 2019',
-        'kat' => 'berita'
-    ],
-    [
-        'gambar' => 'tableware.jpg',
-        'judul' => 'Sale & Pameran Plastic Lucky Star di PRJ Expo Jakarta 2019',
-        'kat' => 'berita'
-    ],
-    [
-        'gambar' => 'container.jpg',
-        'judul' => 'Apa saja istilah di dunia plastik, dan mengapa penting mengetahuinya?',
-        'kat' => 'artikel'
-    ]
-];
-?>
-
-<section class="home-sec-4">
-	<div class="prelative container">
-		<div class="row">
-			<div class="col-md-60">
-				<div class="title">
-					<p>Blog Terbaru</p>
-				</div>
-			</div>
-			<?php foreach($mod_blog as $key => $value): ?>
-			<div class="col-md-15">
-				<div class="box-content">
-					<div class="image"><img src="<?php echo $this->assetBaseurl; ?><?php echo $value['gambar'] ?>" alt=""></div>
-					<div class="kategori">
-						<p><?php echo $value['kat'] ?></p>
-					</div>
-					<div class="judul">
-						<p><?php echo $value['judul'] ?></p>
-					</div>
-				</div>
-			</div>
-			<?php endforeach ?>
-		</div>
-	</div>
-</section>
-
-<section class="home-sec-5">
-	<div class="prelative container">
-		<div class="row">
-			<div class="col-md-60">
-				<div class="title">
-					<p>mari berbicara dengan kami</p>
-				</div>
-			</div>
-		</div>
-		<form action=""></form>
-	</div>
+    <div class="prelative container">
+        <div class="row">
+            <div class="col-md-60">
+                <div class="box-content quality">
+                    <div class="title">
+                        <h4>Reich Standard Quality & Advantage</h4>
+                    </div>
+                    <div class="subtitle">
+                        <p>4 basic concepts that act as a company and product characteristicof Reich.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-15">
+                <div class="box-content">
+                    <div class="image">
+                        <img src="<?php echo $this->assetBaseurl; ?>quality1.png" alt="">
+                    </div>
+                    <div class="caption">
+                        <p>Never ending creative Inovation, development and creation.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-15">
+                <div class="box-content">
+                    <div class="image">
+                        <img src="<?php echo $this->assetBaseurl; ?>quality2.png" alt="">
+                    </div>
+                    <div class="caption">
+                        <p>Consistency in quality and international standard checking.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-15">
+                <div class="box-content">
+                    <div class="image">
+                        <img src="<?php echo $this->assetBaseurl; ?>quality3.png" alt="">
+                    </div>
+                    <div class="caption">
+                        <p>Good business plan for profitable partnership and distribution.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-15">
+                <div class="box-content">
+                    <div class="image">
+                        <img src="<?php echo $this->assetBaseurl; ?>quality4.png" alt="">
+                    </div>
+                    <div class="caption">
+                        <p>Optimum value that exceeds price and expectations.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </section>
--- protected/views/layouts/_footer.php
+++ protected/views/layouts/_footer.php
@@ -1,32 +1,54 @@
 <section class="footer">
     <div class="prelative container">
-        <div class="image-top"><img src="" alt=""></div>
-        <div class="sosmed">
-            <div class="ul">
-                <li></li>
-                <li></li>
-                <li></li>
-            </div>
-        </div>
-        <div class="alamat">
-            <p></p>
-        </div>
-        <div class="email"><a href=""></a></div>
-        <div class="open">
-            <p></p>
-        </div>
-    </div>
-</section>
-
-<section class="footer-sec-2">
-    <div class="prelative container">
         <div class="row">
-            <div class="col-md-60">
-                <div class="copy">
-                    <p>Copyright © 2019 PT. Gunung Agung Sentosa - Lucky Star Plastics. Gresik, Surabaya - East Java, Indonesia. </p>
+            <div class="col-md-25">
+                <div class="box-content">
+                    <div class="image">
+                        <img src="<?php echo $this->assetBaseurl; ?>logo-footer.png" alt="">
+                        <p>Reich Architectural & Furniture Accessories</p>
+                    </div>
                 </div>
-                <div class="by"><a href="#">Website design by Markdesign.</a></div>
             </div>
         </div>
+        <div class="row">
+            <div class="col-md-8">
+                <div class="box-content menu">
+                    <p>Our Product Collections</p>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="box-content menu">
+                    <p>About Us</p>
+                </div>
+            </div>
+            <div class="col-md-5">
+                <div class="box-content menu">
+                    <p>Our Quality</p>
+                </div>
+            </div>
+            <div class="col-md-5">
+                <div class="box-content menu">
+                    <p>Hot To Buy</p>
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div class="box-content menu">
+                    <p>Stores</p>
+                </div>
+            </div>
+            <div class="col-md-5">
+                <div class="box-content menu">
+                    <p>Partnership</p>
+                </div>
+            </div>
+            <div class="col-md-30">
+                <div class="box-content wa">
+                    <p>Whatsapp Hotline & Chat</p>
+                    <img class="wa-footer" src="<?php echo $this->assetBaseurl; ?>wa-logo-footer.png" alt="">
+                    <p>081 5530 78875 (Click To Chat)</p>
+                </div>
+            </div>
+        </div>
+        <div class="garis-formhome"></div>
     </div>
 </section>
